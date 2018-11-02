6f2c76463bf5acb2098abe238878f27c73966ee6
Joshua <joshua.c4mpbell@gmail.com>
2018-11-02 15:35:49 +0000

    finished proj

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('owl.html', { root:  'public' });
+});
+
+router.get('/integer', function(req, res ,next){
+    var url = "http://numbersapi.com/";
+    url += req.query['q'];
+    url += "?json";
+    request(url).pipe(res);
+});
+  
+module.exports = router;
